2c7a42676874d8969869a97f2af6d9228d76bf40
MarioxMor <mario.moralesgu@my.unitec.edu.mx>
2024-10-07 12:11:31 -0600

    Update StringsEjercicios.cpp

--- Introduccion/StringsEjercicios.cpp
+++ Introduccion/StringsEjercicios.cpp
@@ -10,101 +10,105 @@
 using namespace std;
 
 int main() {
-    // Manejo de punto decimal y separador de miles
-    locale loc("");
-    cout.imbue(loc);
+	// Manejo de punto decimal y separador de miles
+	locale loc("");
+	cout.imbue(loc);
 
-    // Declacion e imprasion de String // 
-    string  Texto0 = "Hola Mundo!!";
-    cout << Texto0 << endl;
-
-
-    // Unir Dos textos en uno// 
-    string primertexto = "Hola ";
-    string segundotexto = "Mundo!";
-    string textounido = primertexto + segundotexto;
-    cout << textounido << endl;
+	// Declacion e imprasion de String // 
+	string  Texto0 = "Hola Mundo!!";
+	cout << Texto0 << endl;
 
 
-    // Introducir un valor en una variable(string)//
-
-    string nombre;
-    cout << "Introduce tu nombre: ";
-    cin >> nombre;
-    cout << "Hola " << nombre << "!" << endl;
-
-
-    //EJERCICIO NUMERO  4 //
-
-    // Introducir una lina de texto completa// 
-
-    cin.ignore(); //ignora la instruccion cin PREVIOS// 
-    string NombreCompleto;
-    cout << "Introduce tu nombre completo: ";
-    getline(cin, NombreCompleto); //obtiene una linea de texto //
-    cout << "Tu Nombre completo: " << NombreCompleto << endl; 
+	// Unir Dos textos en uno// 
+	string primertexto = "Hola ";
+	string segundotexto = "Mundo!";
+	string textounido = primertexto + segundotexto;
+	cout << textounido << endl;
 
 
+	// Introducir un valor en una variable(string)//
+
+	string nombre;
+	cout << "Introduce tu nombre: ";
+	cin >> nombre;
+	cout << "Hola " << nombre << "!" << endl;
 
 
-    // De que tamaño es un string// 
-    cout << "La longitud del texto es: " << NombreCompleto.length() << endl;
+	//EJERCICIO NUMERO  4 //
 
-    // cuando hay un "." significa operador de acceso y cuando haya "[]" significa Operador de indice//
+		// Introducir una lina de texto completa// 
 
-    // Ejercicio 6
-
-    // Acceder a un caracter especifico
-    cout << "El primer caracater de nombre completo es: " << NombreCompleto[0] << endl;
-
-    // Ejercicio 7//
-    // Cambiar un carácter específico en un texto (string)
-
-    string cambio = "Buenas";
-    cambio[0] = 'b';  
-    cout << cambio << endl;
-
-    // Ejercicio 8
-    // Convertir un string a mayúsculas
-
-    string textominusculas = "texto a convertir";
-    transform(textominusculas.begin(), textominusculas.end(), textominusculas.begin(), ::toupper);
-    cout << "Texto en mayúsculas: " << textominusculas << endl;
-
-    // Ejercicio 9
-    // Convertir un texto a minúsculas
-
-    string textoMayusculas = "TEXTO A CONVERTIR";
-    transform(textoMayusculas.begin(), textoMayusculas.end(), textoMayusculas.begin(),::tolower);
-    cout << "Texto en minúsculas: " << textoMayusculas << endl;
-    
-    //Ejercicio 10// 
-    //Insertar texto  de otro texto
-
-    string  textoPrincipal = "Mario"; 
-    textoPrincipal.insert(6, "Alejandro");
-        cout << textoPrincipal << endl;
-
-    // Ej 11// 
-
-    // Revertir texto
-        string textoRevertir = "Hola Mundo!";
-        reverse(textoRevertir.begin(), textoRevertir.end());
-        cout << textoRevertir << endl;
-
-        // Ejercicio 12 // 
-        // Convertir numero a tring// 
-
-        int nl = 123;
-        string numeroATexto = to_string(nl);
-            cout << "NUmero como string" << numeroATexto << endl;
+	cin.ignore(); //ignora la instruccion cin PREVIOS// 
+	string NombreCompleto;
+	cout << "Introduce tu nombre completo: ";
+	getline(cin, NombreCompleto); //obtiene una linea de texto //
+	cout << "Tu Nombre completo: " << NombreCompleto << endl;
 
 
-    // Ejercicio 13// 
-     // String a numero// 
-            string textoANumero = "123"; 
-            int n2 = stoi(textoANumero);
-            cout << "String como numero" << n2 << endl;
 
-    return 0;
+
+	// De que tamaño es un string// 
+	cout << "La longitud del texto es: " << NombreCompleto.length() << endl;
+
+	// cuando hay un "." significa operador de acceso y cuando haya "[]" significa Operador de indice//
+
+	// Ejercicio 6
+
+	// Acceder a un caracter especifico
+	cout << "El primer caracater de nombre completo es: " << NombreCompleto[0] << endl;
+
+
+	//Ejecicio 7// 
+	//cambiar un caracter especifico en un texto ( string)// 
+
+
+
+	string cambio = "Buenas";
+	cambio[0] = 'b';
+	cout << cambio << endl;
+
+	// Ejercicio 8
+	// Convertir un string a mayúsculas
+
+	string textominusculas = "texto a convertir";
+	transform(textominusculas.begin(), textominusculas.end(), textominusculas.begin(), ::toupper);
+	cout << "Texto en mayúsculas: " << textominusculas << endl;
+
+	// Ejercicio 9
+	// Convertir un texto a minúsculas
+
+	string textoMayusculas = "TEXTO A CONVERTIR";
+	transform(textoMayusculas.begin(), textoMayusculas.end(), textoMayusculas.begin(), ::tolower);
+	cout << "Texto en minúsculas: " << textoMayusculas << endl;
+
+	//Ejercicio 10// 
+	//Insertar texto  de otro texto
+
+	string  textoPrincipal = "Mario";
+	textoPrincipal.insert(6, "Alejandro");
+	cout << textoPrincipal << endl;
+
+	// EJERCICIO 11// 
+
+	// Revertir texto
+	string textoRevertir = "Hola Mundo!";
+	reverse(textoRevertir.begin(), textoRevertir.end());
+	cout << textoRevertir << endl;
+
+	// Ejercicio 12 // 
+	// Convertir numero a tring// 
+
+	int nl = 123;
+	string numeroATexto = to_string(nl);
+	cout << "NUmero como string" << numeroATexto << endl;
+
+
+	// Ejercicio 13// 
+			// String a numero// 
+	string textoANumero = "123";
+	int n2 = stoi(textoANumero);
+	cout << "String como numero" << n2 << endl;
+
+	return 0;
 }
+
